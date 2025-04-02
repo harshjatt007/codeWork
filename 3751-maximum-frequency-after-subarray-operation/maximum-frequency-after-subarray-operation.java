d506d85339f7472f5be691b85f1724278c2e4bc0
Abhishek Choudhary <122774453+harshjatt007@users.noreply.github.com>
2025-04-02 21:11:12 +0530

    Time: 48 ms (70.17%) | Memory: 57.1 MB (83.62%) - LeetSync

--- 3751-maximum-frequency-after-subarray-operation/maximum-frequency-after-subarray-operation.java
+++ 3751-maximum-frequency-after-subarray-operation/maximum-frequency-after-subarray-operation.java
@@ -1,10 +1,43 @@
+// class Solution {
+//         public int maxFrequency(int[] A, int k) {
+//         int count[] = new int[51], res = 0;
+//         for (int a : A) {
+//             count[a] = Math.max(count[a], count[k]) + 1;
+//             res = Math.max(res, count[a] - count[k]);
+//         }
+//         return count[k] + res;
+//     }
+// }
+
 class Solution {
-        public int maxFrequency(int[] A, int k) {
-        int count[] = new int[51], res = 0;
+    public int maxFrequency(int[] A, int k) {
+        Map<Integer, Integer> count = new HashMap<>();
         for (int a : A) {
-            count[a] = Math.max(count[a], count[k]) + 1;
-            res = Math.max(res, count[a] - count[k]);
+            count.put(a, count.getOrDefault(a, 0) + 1);
         }
-        return count[k] + res;
+
+        int res = 0;
+        for (int b : count.keySet()) {
+            res = Math.max(res, kadane(A, k, b));
+        }
+
+        return count.getOrDefault(k, 0) + res;
+    }
+
+    private int kadane(int[] A, int k, int b) {
+        int res = 0, cur = 0;
+        for (int a : A) {
+            if (a == k) {
+                cur--;
+            }
+            if (a == b) {
+                cur++;
+            }
+            if (cur < 0) {
+                cur = 0;
+            }
+            res = Math.max(res, cur);
+        }
+        return res;
     }
 }
