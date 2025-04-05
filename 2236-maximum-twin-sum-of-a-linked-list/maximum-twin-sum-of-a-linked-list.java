9a8cc15e18adc00bded4b8cffeaaa077b44f51c3
Abhishek Choudhary <122774453+harshjatt007@users.noreply.github.com>
2025-04-05 16:15:16 +0530

    Time: 4 ms (97.34%) | Memory: 63.6 MB (72.63%) - LeetSync

--- 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.java
+++ 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.java
@@ -8,19 +8,55 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// class Solution {
+//     public int pairSum(ListNode head) {
+//         Stack<Integer>stack=new Stack<>();
+//         ListNode dummy=head;
+//         while(dummy!=null){
+//             stack.push(dummy.val);
+//             dummy=dummy.next;
+//         }
+//         int maxTwinSum=Integer.MIN_VALUE;
+//         while(head!=null){
+//             maxTwinSum=Math.max(maxTwinSum,head.val+stack.pop());
+//             head=head.next;
+//         }
+//         return maxTwinSum;
+//     }
+// }
+
 class Solution {
     public int pairSum(ListNode head) {
-        Stack<Integer>stack=new Stack<>();
-        ListNode dummy=head;
-        while(dummy!=null){
-            stack.push(dummy.val);
-            dummy=dummy.next;
+        ListNode fast=head;
+        ListNode slow=head;
+       
+
+        while(fast!=null && fast.next!=null){
+            fast=fast.next.next;
+            slow=slow.next;
         }
-        int maxTwinSum=Integer.MIN_VALUE;
-        while(head!=null){
-            maxTwinSum=Math.max(maxTwinSum,head.val+stack.pop());
-            head=head.next;
+
+        ListNode prev=null;
+
+        ListNode current=slow;
+
+        while(current!=null){
+            ListNode next=current.next;
+            current.next=prev;
+            prev=current;
+            current=next;
         }
-        return maxTwinSum;
+
+        ListNode start=head;
+        int sum=0;
+
+        while(prev!=null){
+         sum=Math.max(prev.val+start.val,sum);
+         prev=prev.next;
+         start=start.next;
+        }
+
+        return sum;
+
     }
 }
