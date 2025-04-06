fc1964dc860a58c5939fed555b391c068da21bd2
Abhishek Choudhary <122774453+harshjatt007@users.noreply.github.com>
2025-04-06 23:44:14 +0530

    Time: 1 ms (98.39%) | Memory: 44.7 MB (78.05%) - LeetSync

--- 1168-duplicate-zeros/duplicate-zeros.java
+++ 1168-duplicate-zeros/duplicate-zeros.java
@@ -1,24 +1,43 @@
+// class Solution {
+//     public void duplicateZeros(int[] arr) {
+//         int nums[]=new int[arr.length];
+//         int i=0;
+//         int index=0;
+//         while(i<arr.length){
+//             if(arr[index]!=0){
+//                 nums[i]=arr[index];
+//                 i++;
+//                 index++;
+//             }
+//             else{
+//                 nums[i]=0;
+//                 index++;
+//                 if(i+1<arr.length){
+//                 nums[i+1]=0;
+//                 i+=2;
+//                 }
+//                 else i++;
+//             }
+//         }
+//         System.arraycopy(nums,0,arr,0,arr.length);
+//     }
+// }
 class Solution {
     public void duplicateZeros(int[] arr) {
-        int nums[]=new int[arr.length];
-        int i=0;
-        int index=0;
-        while(i<arr.length){
-            if(arr[index]!=0){
-                nums[i]=arr[index];
-                i++;
-                index++;
+        int length = arr.length;
+        int[] temp = Arrays.copyOf(arr, length);
+        int i = 0;
+        int j = 0;
+        
+        while (i < length) {
+            arr[i] = temp[j];
+            
+            if (temp[j] == 0 && i < length - 1) {
+                arr[++i] = 0;
             }
-            else{
-                nums[i]=0;
-                index++;
-                if(i+1<arr.length){
-                nums[i+1]=0;
-                i+=2;
-                }
-                else i++;
-            }
+            
+            i++;
+            j++;
         }
-        System.arraycopy(nums,0,arr,0,arr.length);
     }
 }
