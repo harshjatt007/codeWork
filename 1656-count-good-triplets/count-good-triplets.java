cd0fec12ef1cd4bf9891073bd7cfec84acf9fdb8
Abhishek Choudhary <122774453+harshjatt007@users.noreply.github.com>
2025-04-14 15:34:06 +0530

    Time: 12 ms (59.83%) | Memory: 41.7 MB (24.01%) - LeetSync

--- 1656-count-good-triplets/count-good-triplets.java
+++ 1656-count-good-triplets/count-good-triplets.java
@@ -1,21 +1,21 @@
-// class Solution {
-//     public int countGoodTriplets(int[] arr, int a, int b, int c) {
-//         int count=0;
-//         for(int i=0;i<arr.length;i++){
-//             for(int j=i+1;j<arr.length;j++){
-//                 if (Math.abs(arr[i] - arr[j]) > a) continue;
-//                 for(int k=j+1;k<arr.length;k++){
-//                     if(Math.abs(arr[i]-arr[j])<=a && Math.abs(arr[j]-arr[k])<=b && Math.abs(arr[i]-arr[k])<=c){
-//                         count++;
-//                     }
-//                 }
-//             }
-//         }
-//         return count;
-//     }
-// }
+class Solution {
+    public int countGoodTriplets(int[] arr, int a, int b, int c) {
+        int count=0;
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if (Math.abs(arr[i] - arr[j]) > a) continue;
+                for(int k=j+1;k<arr.length;k++){
+                    if(Math.abs(arr[i]-arr[j])<=a && Math.abs(arr[j]-arr[k])<=b && Math.abs(arr[i]-arr[k])<=c){
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+}
 
-
+/*
 class Solution {
     public int countGoodTriplets(int[] arr, int a, int b, int c) {
         int n = arr.length;
@@ -54,3 +54,4 @@ class Solution {
         return totalTriplets;
     }
 }
+*/
