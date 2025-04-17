c3b86135b250290ab79757b202e70ec1b84dfcaf
Abhishek Choudhary <122774453+harshjatt007@users.noreply.github.com>
2025-04-17 23:53:20 +0530

    Time: 7 ms (30.52%) | Memory: 45.3 MB (19.68%) - LeetSync

--- 1232-sum-of-mutated-array-closest-to-target/sum-of-mutated-array-closest-to-target.java
+++ 1232-sum-of-mutated-array-closest-to-target/sum-of-mutated-array-closest-to-target.java
@@ -1,51 +1,52 @@
-// class Solution {
-//     public int findBestValue(int[] arr, int target) {
-//         Arrays.sort(arr);
-//         int n = arr.length;
-//         int[] prefix = new int[n + 1];
-//         for (int i = 0; i < n; ++i) {
-//             prefix[i + 1] = prefix[i] + arr[i];
-//         }
+class Solution {
+    public int findBestValue(int[] arr, int target) {
+        Arrays.sort(arr);
+        int n = arr.length;
+        int[] prefix = new int[n + 1];
+        for (int i = 0; i < n; ++i) {
+            prefix[i + 1] = prefix[i] + arr[i];
+        }
         
-//         int left = 0;
-//         int right = arr[n - 1];
-//         int bestValue = 0;
-//         int minDiff = Integer.MAX_VALUE;
+        int left = 0;
+        int right = arr[n - 1];
+        int bestValue = 0;
+        int minDiff = Integer.MAX_VALUE;
         
-//         while (left <= right) {
-//             int mid = left + (right - left) / 2;
-//             int index = binarySearch(arr, mid);
-//             int sum = prefix[index] + (n - index) * mid;
-//             int diff = Math.abs(sum - target);
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int index = binarySearch(arr, mid);
+            int sum = prefix[index] + (n - index) * mid;
+            int diff = Math.abs(sum - target);
             
-//             if (diff < minDiff || (diff == minDiff && mid < bestValue)) {
-//                 minDiff = diff;
-//                 bestValue = mid;
-//             }
+            if (diff < minDiff || (diff == minDiff && mid < bestValue)) {
+                minDiff = diff;
+                bestValue = mid;
+            }
             
-//             if (sum < target) {
-//                 left = mid + 1;
-//             } else {
-//                 right = mid - 1;
-//             }
-//         }
-//         return bestValue;
-//     }
+            if (sum < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return bestValue;
+    }
     
-//     private int binarySearch(int[] arr, int target) {
-//         int left = 0;
-//         int right = arr.length;
-//         while (left < right) {
-//             int mid = left + (right - left) / 2;
-//             if (arr[mid] > target) {
-//                 right = mid;
-//             } else {
-//                 left = mid + 1;
-//             }
-//         }
-//         return left;
-//     }
-// }
+    private int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] > target) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
+/*
 class Solution {
     public int findBestValue(int[] A, int target) {
         Arrays.sort(A);
@@ -60,3 +61,4 @@ class Solution {
         return res;
     }
 }
+*/
