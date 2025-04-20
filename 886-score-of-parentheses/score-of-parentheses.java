9be6c551561e51a4b7d2c786f2cccaef52ca7de4
Abhishek Choudhary <122774453+harshjatt007@users.noreply.github.com>
2025-04-20 16:53:26 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (78.26%) - LeetSync

--- 886-score-of-parentheses/score-of-parentheses.java
+++ 886-score-of-parentheses/score-of-parentheses.java
@@ -1,16 +1,33 @@
+// class Solution {
+//     public int scoreOfParentheses(String s) {
+//     int score=0;
+//     Stack<Integer>stack=new Stack<>();
+//     for(char c:s.toCharArray()){
+//         if(c=='(') {
+//             stack.push(score);
+//             score=0;
+//         }
+//         else{
+//             score=stack.pop()+Math.max(2*score,1);
+//         }
+//     }
+//     return score;
+//     }
+// }
+
+// Greedy Approach
 class Solution {
     public int scoreOfParentheses(String s) {
-    int score=0;
-    Stack<Integer>stack=new Stack<>();
-    for(char c:s.toCharArray()){
-        if(c=='(') {
-            stack.push(score);
-            score=0;
+        int score=0; int depth=0;
+        for(int i=0;i<s.length();i++){
+            int c=s.charAt(i);
+            if(c=='(') depth++;
+            else {
+                depth--;
+                if(s.charAt(i-1)=='(')
+                score+=1<<depth;
+            }
         }
-        else{
-            score=stack.pop()+Math.max(2*score,1);
-        }
-    }
-    return score;
+        return score;
     }
 }
