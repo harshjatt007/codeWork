cf6e5ebb9f7bd486dd67ad65b137701cc5c6eb34
Abhishek Choudhary <122774453+harshjatt007@users.noreply.github.com>
2025-04-23 13:17:28 +0530

    Time: 5 ms (73.12%) | Memory: 40.7 MB (72.76%) - LeetSync

--- 1500-count-largest-group/count-largest-group.java
+++ 1500-count-largest-group/count-largest-group.java
@@ -1,29 +1,53 @@
+// class Solution {
+//     public int digitSum(int n){
+//         int result=0;
+//         n=Math.abs(n);
+//         while(n!=0){
+//             result=result+n%10;
+//             n/=10;
+//         }
+//         return result;
+//     }
+//     public int countLargestGroup(int n) {
+//         int count=0;
+//         HashMap<Integer,Integer>map=new HashMap<>();
+//         for(int i=1;i<=n;i++){
+//             int sum=digitSum(i);
+//             map.put(sum,map.getOrDefault(sum,0)+1);
+//         }
+//         int maxValue=0;
+//         for(int value:map.values()){
+//             if(value>maxValue){
+//                 maxValue=value;
+//                 count=1;
+//             }
+//            else if(value==maxValue) count++;
+//         }
+//         return count;
+//     }
+// }
+
 class Solution {
     public int digitSum(int n){
-        int result=0;
-        n=Math.abs(n);
+        int sum=0;
         while(n!=0){
-            result=result+n%10;
+            sum=sum+n%10;
             n/=10;
         }
-        return result;
+        return sum;
     }
     public int countLargestGroup(int n) {
-        int count=0;
-        HashMap<Integer,Integer>map=new HashMap<>();
+        int freq[]=new int[37];
+        int maxGroupSize=0;
         for(int i=1;i<=n;i++){
             int sum=digitSum(i);
-            map.put(sum,map.getOrDefault(sum,0)+1);
+            freq[sum]++;
+            maxGroupSize=Math.max(maxGroupSize,freq[sum]);
         }
-        int maxValue=0;
-        for(int value:map.values()){
-            if(value>maxValue){
-                maxValue=value;
-                count=1;
-            }
-           else if(value==maxValue) count++;
+        int count=0;
+        for(int value:freq){
+            if(value==maxGroupSize) count++;
         }
         return count;
     }
 }
-
