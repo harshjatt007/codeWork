cac7dec13bb03a7004f6606b03b13b78a6656255
Abhishek Choudhary <122774453+harshjatt007@users.noreply.github.com>
2025-05-04 15:16:03 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (49.75%) - LeetSync

--- 1944-truncate-sentence/truncate-sentence.java
+++ 1944-truncate-sentence/truncate-sentence.java
@@ -1,12 +1,22 @@
+// class Solution {
+//     public String truncateSentence(String s, int k) {
+//         String words[]=s.split(" ");
+//         if(k>words.length) return "-1";
+//         String ans="";
+//         for(int i=0;i<k;i++){
+//             ans+=words[i];
+//             if(i!=k-1) ans+=" ";
+//         }
+//         return ans;
+//     }
+// }
+
 class Solution {
     public String truncateSentence(String s, int k) {
-        String words[]=s.split(" ");
-        if(k>words.length) return "-1";
-        String ans="";
-        for(int i=0;i<k;i++){
-            ans+=words[i];
-            if(i!=k-1) ans+=" ";
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)==' ') k--;
+            if(k==0) return s.substring(0,i);
         }
-        return ans;
+        return s;
     }
 }
