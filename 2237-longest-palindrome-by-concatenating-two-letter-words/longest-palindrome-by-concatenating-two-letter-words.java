717c38309a7c12675e14facc2568cb3e7251f939
Abhishek Choudhary <122774453+harshjatt007@users.noreply.github.com>
2025-05-25 13:18:08 +0530

    Time: 14 ms (92.28%) | Memory: 61 MB (8.74%) - LeetSync

--- 2237-longest-palindrome-by-concatenating-two-letter-words/longest-palindrome-by-concatenating-two-letter-words.java
+++ 2237-longest-palindrome-by-concatenating-two-letter-words/longest-palindrome-by-concatenating-two-letter-words.java
@@ -1,62 +1,31 @@
-// class Solution {
-//     public int longestPalindrome(String[] words) {
-//         Map<String, Integer> map = new HashMap<>();
-//         int count = 0;
-//         boolean hasSinglePalindrome = false;
-
-//         for (String word : words) {
-//             map.put(word, map.getOrDefault(word, 0) + 1);
-//         }
-
-//         for (String word : map.keySet()) {
-//             String reversed = new StringBuilder(word).reverse().toString();
-
-//             if (!word.equals(reversed) && map.containsKey(reversed)) {
-//                 int pairs = Math.min(map.get(word), map.get(reversed));
-//                 count += pairs * 4;
-//                 map.put(word, map.get(word) - pairs);
-//                 map.put(reversed, map.get(reversed) - pairs);
-//             }
-//         }
-
-//         for (String word : map.keySet()) {
-//             if (word.charAt(0) == word.charAt(1)) {
-//                 int pairs = map.get(word) / 2;
-//                 count += pairs * 4;
-//                 if (map.get(word) % 2 == 1) {
-//                     hasSinglePalindrome = true;
-//                 }
-//             }
-//         }
-
-//         if (hasSinglePalindrome) {
-//             count += 2; // one central palindrome like "gg"
-//         }
-
-//         return count;
-//     }
-// }
-
 class Solution {
     public int longestPalindrome(String[] words) {
-       HashMap<String,Integer>map=new HashMap<>();
-       int count=0;
-       boolean hasSingleMiddle=false;
-       for(String word:words){
-        String rev=""+word.charAt(1)+word.charAt(0);
-        if(map.getOrDefault(rev,0)>0){
-            count+=4;
-            map.put(rev,map.get(rev)-1);
+        int[][] count = new int[26][26];
+        int result = 0;
+        boolean hasMiddle = false;
+
+        for (String word : words) {
+            int a = word.charAt(0) - 'a';
+            int b = word.charAt(1) - 'a';
+
+            if (count[b][a] > 0) {
+                // Found a reverse pair
+                result += 4;
+                count[b][a]--;
+            } else {
+                count[a][b]++;
+            }
         }
-        else map.put(word,map.getOrDefault(word,0)+1);
-       }
-       for(String key:map.keySet()){
-        if(key.charAt(0)==key.charAt(1) && map.get(key)>0){
-            hasSingleMiddle=true;
-            break;
+
+        // Check for possible middle word like "gg"
+        for (int i = 0; i < 26; i++) {
+            if (count[i][i] > 0) {
+                hasMiddle = true;
+                break;
+            }
         }
-       }
-       if(hasSingleMiddle) count+=2;
-       return count;
+
+        if (hasMiddle) result += 2;
+        return result;
     }
 }
