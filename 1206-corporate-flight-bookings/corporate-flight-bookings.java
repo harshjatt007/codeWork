58f9f97465a50e4df7d53d970ad4ccec7394e1ab
Abhishek Choudhary <122774453+harshjatt007@users.noreply.github.com>
2025-06-23 12:45:24 +0530

    Time: 3 ms (97.97%) | Memory: 60 MB (51.88%) - LeetSync

--- 1206-corporate-flight-bookings/corporate-flight-bookings.java
+++ 1206-corporate-flight-bookings/corporate-flight-bookings.java
@@ -1,27 +1,19 @@
 class Solution {
     public int[] corpFlightBookings(int[][] bookings, int n) {
-        ArrayList<Integer> answer = new ArrayList<>(Collections.nCopies(n, 0));
-        
-        // Step 1: Apply the difference array technique
-        for (int[] booking : bookings) {
-            int first = booking[0] - 1;
-            int last = booking[1] - 1;
-            int seats = booking[2];
-            
-            answer.set(first, answer.get(first) + seats);
-            if (last + 1 < n) {
-                answer.set(last + 1, answer.get(last + 1) - seats);
-            }
+        int res[]=new int[n];
+        int start,end,seats;
+        // Ye Difference Array method hai
+        for(int booking[]:bookings){
+            start=booking[0]-1; // -1 for converting it to zero index 
+            end=booking[1]-1;
+            seats=booking[2];
+            res[end]+=seats;
+            if(start>=1) res[start-1]-=seats;
         }
-        
-        // Step 2: Compute prefix sum
-        for (int i = 1; i < n; i++) {
-            answer.set(i, answer.get(i) + answer.get(i - 1));
+        // just taking prefix sum in reverse order
+        for(int i=n-2;i>=0;i--){
+            res[i]+=res[i+1];
         }
-        int[] arr = new int[answer.size()]; 
-        for (int i = 0; i < answer.size(); i++) {
-            arr[i] = answer.get(i);
-        }        
-        return arr;
+        return res;
     }
 }
