16d546b1771f25ecb2ff1e561a552eae7d0cd8cc
Abhishek Choudhary <122774453+harshjatt007@users.noreply.github.com>
2025-07-21 22:43:02 +0530

    Time: 4 ms (52.11%) | Memory: 42 MB (43.93%) - LeetSync

--- 97-interleaving-string/interleaving-string.java
+++ 97-interleaving-string/interleaving-string.java
@@ -2,20 +2,18 @@ class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
         int m=s1.length(),n=s2.length(),k=s3.length();
         if(m+n!=k) return false;
-        Boolean memo[][]=new Boolean[m+1][n+1];
-        return helper(s1,0,s2,0,s3,0,memo);
-    }
-    public boolean helper(String s1,int i,String s2,int j,String s3,int k,Boolean [][]memo){
-        if(memo[i][j]!=null) return memo[i][j];
-        if(k==s3.length()) return i==s1.length() && j==s2.length();
-        boolean ans=false;
-        if(i<s1.length() && s1.charAt(i)==s3.charAt(k)){
-            ans=ans || helper(s1,i+1,s2,j,s3,k+1,memo);
+        boolean dp[][]=new boolean[m+1][n+1];
+        dp[0][0]=true;
+        for(int i=0;i<=m;i++){
+            for(int j=0;j<=n;j++){
+               if(i>0){
+                dp[i][j]|=dp[i-1][j] && s1.charAt(i-1)==s3.charAt(i+j-1);
+               }
+               if(j>0){
+                dp[i][j]=dp[i][j] || dp[i][j-1] && s2.charAt(j-1)==s3.charAt(i+j-1);
+               }
+            }
         }
-        if(j<s2.length() && s2.charAt(j)==s3.charAt(k)){
-            ans=ans || helper(s1,i,s2,j+1,s3,k+1,memo);
-        }
-        memo[i][j]=ans;
-        return ans;
+        return dp[m][n];
     }
 }
