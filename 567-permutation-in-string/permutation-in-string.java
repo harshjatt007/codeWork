19e4f263b599063e0bd1ea3cdf6565eb3ed43fb0
Abhishek Choudhary <122774453+harshjatt007@users.noreply.github.com>
2025-07-24 14:56:51 +0530

    Time: 8 ms (60.62%) | Memory: 42.8 MB (57.89%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,13 +1,27 @@
-class Solution {
+public class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        char c[]=s1.toCharArray();
-        Arrays.sort(c);
-        for(int i=0;i<=s2.length()-s1.length();i++){
-            String str=s2.substring(i,i+s1.length());
-            char ch[]=str.toCharArray();
-            Arrays.sort(ch);
-            if(Arrays.equals(ch,c)) return true;
+        if (s1.length() > s2.length())
+            return false;
+        int[] s1arr = new int[26];
+        int[] s2arr = new int[26];
+        for (int i = 0; i < s1.length(); i++) {
+            s1arr[s1.charAt(i) - 'a']++;
+            s2arr[s2.charAt(i) - 'a']++;
         }
-        return false;
+        for (int i = 0; i < s2.length() - s1.length(); i++) {
+            if (matches(s1arr, s2arr))
+                return true;
+            s2arr[s2.charAt(i + s1.length()) - 'a']++;
+            s2arr[s2.charAt(i) - 'a']--;
+        }
+        return matches(s1arr, s2arr);
+    }
+    
+    public boolean matches(int[] s1arr, int[] s2arr) {
+        for (int i = 0; i < 26; i++) {
+            if (s1arr[i] != s2arr[i])
+                return false;
+        }
+        return true;
     }
 }
