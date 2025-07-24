62c736acb82b5b09a2b9c5ff8c080c6ff5a37f71
Abhishek Choudhary <122774453+harshjatt007@users.noreply.github.com>
2025-07-24 15:16:13 +0530

    Time: 31 ms (20.92%) | Memory: 45.6 MB (14.57%) - LeetSync

--- 47-permutations-ii/permutations-ii.java
+++ 47-permutations-ii/permutations-ii.java
@@ -3,28 +3,17 @@ class Solution {
         List<List<Integer>>result=new ArrayList<>();
         List<Integer>arr=new ArrayList<>();
         for(int i:nums) arr.add(i);
-         TreeSet<List<Integer>> set = new TreeSet<>((a, b) -> {
-            for (int i = 0; i < a.size(); i++) {
-                if (!a.get(i).equals(b.get(i))) {
-                    return a.get(i) - b.get(i);
-                }
-            }
-            return 0;
-        });
+        HashSet<List<Integer>> set = new HashSet<>();
         permute(set,arr,0);
-        // for(List<Integer>s:set){
-        //     result.add(s);
-        // }
         result.addAll(set);
         return result;
     }
-    public void permute(TreeSet<List<Integer>>set,List<Integer>arr,int start){
+    public void permute(HashSet<List<Integer>>set,List<Integer>arr,int start){
         if(start==arr.size()){
             set.add(new ArrayList<>(arr));
             return;
         }
         for(int i=start;i<arr.size();i++){
-            // List<Integer>sub=new ArrayList<>(arr);
             Collections.swap(arr,i,start);
             permute(set,arr,start+1);
             Collections.swap(arr,i,start);
