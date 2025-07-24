615618f5dc128d086d9e3f2395e87bbc777b68ee
Abhishek Choudhary <122774453+harshjatt007@users.noreply.github.com>
2025-07-24 12:37:21 +0530

    Time: 1 ms (92.64%) | Memory: 44.8 MB (29.91%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,26 +1,21 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>>result=new ArrayList<>();
-        List<Integer>arr=new ArrayList<>();
-        for(int i:nums) arr.add(i);
-        permutations(result,arr,0);
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> current = new ArrayList<>();
+        for (int num : nums) current.add(num);
+        backtrack(0, current, result);
         return result;
     }
-    public void permutations(List<List<Integer>>result,List<Integer>arr,int start){
-       if(start==arr.size()){
+
+    private void backtrack(int start, List<Integer> arr, List<List<Integer>> result) {
+        if (start == arr.size()) {
             result.add(new ArrayList<>(arr));
             return;
-       }
-       for(int i=start;i<arr.size();i++){
-            ArrayList<Integer>arr1=new ArrayList<>(arr);
-            swap(arr1,start,i);
-            permutations(result,arr1,start+1);
-       }
-    }
-    public void swap(List<Integer>arr1,int i,int j){
-        int temp=arr1.get(i);
-        arr1.set(i,arr1.get(j));
-        arr1.set(j,temp);
-        return;
+        }
+        for (int i = start; i < arr.size(); i++) {
+            Collections.swap(arr, start, i);
+            backtrack(start + 1, arr, result);
+            Collections.swap(arr, start, i); 
+        }
     }
 }
