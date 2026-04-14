a725cd28d6c5bedd1731087c17ebcabf28e41faa
Abhishek Choudhary <122774453+harshjatt007@users.noreply.github.com>
2026-04-14 12:04:48 +0530

    Time: 27 ms (95.23%) | Memory: 146.6 MB (71.02%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,16 +1,15 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        if(nums==null || nums.length==0) return new int[0];
+        if(nums.length==0 || nums==null) return new int[0];
+        if(k==1) return nums;
         int n=nums.length;
+        ArrayDeque<Integer>dq=new ArrayDeque<>();
         int result[]=new int[n-k+1];
-        Deque<Integer>dq=new LinkedList<>();
         for(int i=0;i<n;i++){
-            if(!dq.isEmpty() && dq.peekFirst()<i-k+1) dq.pollFirst();
-            while(!dq.isEmpty() && nums[dq.peekLast()]<nums[i]) dq.pollLast();
-            dq.offerLast(i);
-            if(i>=k-1){
-                result[i-k+1]=nums[dq.peekFirst()];
-            }
+            if(!dq.isEmpty() && dq.peekFirst()<=i-k) dq.pollFirst(); // out of window check
+            while(!dq.isEmpty() && nums[dq.peekLast()] < nums[i]) dq.pollLast(); // ensure max element is in the front of dq
+            dq.offerLast(i); // add current element index in dq
+            if(i>=k-1) result[i-k+1]=nums[dq.peekFirst()]; // store max elem once onwards when we reach window size of k 
         }
         return result;
     }
